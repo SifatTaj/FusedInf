99004801e1ed9e4d8fb98e31c179c3485ff54900
Taj <sifat.taj@gmail.com>
2024-10-16 13:32:30 -0400

    init

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+import torch
+
 from core.compiler import fusedinf_compiler
 from models import *
 from models import ResNet18, vgg16
@@ -9,13 +11,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_dir', type=str, default='models')
     parser.add_argument('--input_dir', type=str, default='inputs')
+    parser.add_argument('--device', type=str, default='cpu')
 
     args = parser.parse_args()
     model_dir = args.model_dir
+    input_dir = args.input_dir
 
     files = os.listdir(model_dir)
+    input_files = os.listdir(input_dir)
 
     models = []
+    inputs = []
 
     for file in files:
         if file.endswith('.py'):
@@ -23,4 +29,13 @@ if __name__ == '__main__':
             exec(f'net = {exec_str}')
             models.append(net)
 
+    for file in input_files:
+        if file.endswith('.pth'):
+            input = torch.load(f'{input_dir}/{file}')
+            input.to(device=args.device)
+            inputs.append(inputs)
+
     compiled_model = fusedinf_compiler(models)
+    compiled_model.eval()
+    compiled_model.to(args.device)
+    print(compiled_model(inputs))
